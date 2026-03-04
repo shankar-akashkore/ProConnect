4666d2957fb7455decc5b1acdc776cbf8d731bd9
shankar akashkore <shankarakashkore32@gmail.com>
2026-03-04 21:23:08 +0530

    Redux and Authentication

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -101,7 +101,7 @@ export const login = async (req,res) => {
 
         await User.updateOne({_id: user._id}, {token});
 
-        return res.json({token});
+        return res.json({token : token});
     } catch(error) {
         return res.status(500).json({message: "Something went wrong"})
     }
---
+++ frontend/src/config/redux/reducer/authReducer/index.js
@@ -0,0 +1,75 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { loginUser, registerUser } from "../../action/authAction";
+
+
+const initialState = {
+    user: [],
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    loggedIn: false,
+    message: "",
+    profileFetched: false,
+    connections: [],
+    connectionRequest: [],
+}
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        reset: () => initialState,
+        handleLoginUser: (state) => {
+            state.message = "hello"
+        }
+    },
+
+    extraReducer: (builder) => {
+
+        builder
+        .addCase(loginUser.pending, (state) => {
+            state.isLoading = true;
+            state.message = "knocking the door......"
+        })
+
+        .addCase(loginUser.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.isError = false;
+            state.isSuccess = true;
+            state.loggedIn = true;
+            state.message = "Login Successful"
+        })
+
+        .addCase(loginUser.rejected, (state, action) => {
+            state.isLoading = false;
+            state.isError = true;
+            state.message = action.payload
+        })
+
+        .addCase(registerUser.pending, (state) => {
+            state.isLoading = true;
+            state.message = "Registering you.........."
+        })
+
+        .addCase(registerUser.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.isError = false;
+            state.isSuccess = true;
+            state.loggedIn = true;
+            state.message = "Registration is Successful"
+        })
+
+        .addCase(registerUser.rejected, (state, action) => {
+            state.isLoading = false;
+            state.isError = true;
+            state.message = action.payload
+        })
+    }
+
+})
+
+
+
+
+
+export default authSlice.reducer;
