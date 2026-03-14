333ffc5a7efaf4c6f49c629b68d7fee39fea0e1b
shankar akashkore <shankarakashkore32@gmail.com>
2026-03-14 21:57:42 +0530

    Creating pdf download functionality

--- frontend/src/pages/view_profile/[username].jsx
+++ frontend/src/pages/view_profile/[username].jsx
@@ -78,6 +78,7 @@ export default function ViewProfile({ userProfile }) {
 
                 </div>
 
+                <div style={{display: "flex", alignItems: "center", gap:"1.2rem"}}>
                 {isCurrentUserInConnections ?
                   <button className={styles.connectedButton}>{isConnectionNull ? "Pending" : "Connected"}</button>
                   :
@@ -86,6 +87,18 @@ export default function ViewProfile({ userProfile }) {
                   }}
                     className={styles.connectBtn}>Connect</button>
                 }
+
+                <div onClick={async () => {
+                  const response = await clientServer.get(`/user/download_resume?id=${userProfile.userId._id}`);
+                  window.open(`${BASE_URL}/${response.data.message}`, "_blank");
+                }}
+                style={{cursor: "pointer"}}>
+                  <svg style={{width: "1.3em"}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                  </svg>
+
+                </div>
+                </div>
 
                 <div>
                   <p>{userProfile.bio}</p>
