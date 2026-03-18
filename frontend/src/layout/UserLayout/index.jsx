646607ccdb5e41e401e46639aa35c48670651579
shankar akashkore <shankarakashkore32@gmail.com>
2026-03-19 02:28:04 +0530

    Navbar Component flow

--- frontend/src/layout/UserLayout/index.jsx
+++ frontend/src/layout/UserLayout/index.jsx
@@ -1,3 +1,22 @@
+// import FooterComponent from '@/components/FooterComponent';
+// import NavbarComponent from '@/components/NavbarComponent';
+// import React from 'react'
+
+// function UserLayout({ children }) {
+//   return (
+//     <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+//       <NavbarComponent />
+//         <div style={{ flex: 1 }}>{children}</div>
+//       <FooterComponent />
+//     </div>
+//   )
+// }
+
+// export default UserLayout;
+
+
+
+
 import FooterComponent from '@/components/FooterComponent';
 import NavbarComponent from '@/components/NavbarComponent';
 import React from 'react'
@@ -6,7 +25,7 @@ function UserLayout({ children }) {
   return (
     <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
       <NavbarComponent />
-        <div style={{ flex: 1 }}>{children}</div>
+        <div style={{ flex: 1, paddingTop: '72px' }}>{children}</div>
       <FooterComponent />
     </div>
   )
