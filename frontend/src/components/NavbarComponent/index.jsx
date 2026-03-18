1e96c7f497d83d085df2224c31ef9ddd4113a0cd
shankar akashkore <shankarakashkore32@gmail.com>
2026-03-18 14:59:16 +0530

    Navbar UI style

--- frontend/src/components/NavbarComponent/index.jsx
+++ frontend/src/components/NavbarComponent/index.jsx
@@ -16,26 +16,25 @@ export default function NavbarComponent() {
     <div className={styles.container}>
       <nav className={styles.navBar}>
 
-        <h3 onClick={() => {
+        <h3 className={styles.logo} onClick={() => {
           router.push('/')
-        }} style={{cursor: "pointer"}}>ProConnect</h3>
+        }}>ProConnect</h3>
 
-        <div className="navBarOptionContainer">
+        <div className={styles.navBarOptionContainer}>
 
           {authState.profileFetched && <div>
-            <div style={{display: "flex", gap: "1.2rem"}}>
-              <p>Hey, {authState.user.userId.name}</p>
-              <p onClick={() => {
+            <div className={styles.userInfoContainer}>
+              <p className={styles.greeting}>Hey, {authState.user.userId.name}</p>
+              <p className={styles.navLink} onClick={() => {
                 router.push('/profile');
-              }}
-              style={{fontWeight: "bold", cursor: "pointer"}}>Profile</p>
-              <p onClick={() => {
+              }}>Profile</p>
+              <p className={styles.navLink} onClick={() => {
                 localStorage.removeItem('token');
                 router.push('/login');
                 dispatch(reset());
-              }} style={{fontWeight: "bold", cursor: "pointer"}}>LogOut</p>
+              }}>LogOut</p>
             </div>
-            </div>}
+          </div>}
 
 
 
@@ -45,7 +44,7 @@ export default function NavbarComponent() {
             <p>Be a part</p>
           </div>}
 
-          
+
         </div>
 
       </nav>
