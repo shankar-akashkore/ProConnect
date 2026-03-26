60ab1838713f24e5b50be1472ff344cd04cc9b76
shankar akashkore <shankarakashkore32@gmail.com>
2026-03-27 02:14:04 +0530

    Styling the dashboard page

--- frontend/src/layout/DashboardLayout/index.jsx
+++ frontend/src/layout/DashboardLayout/index.jsx
@@ -1,6 +1,7 @@
 import React, { Children } from 'react'
 import styles from "./index.module.css";
 import { useRouter } from 'next/router';
+import { BASE_URL } from '@/config';
 import { useEffect } from 'react';
 import { setTokenThere } from '@/config/redux/reducer/authReducer';
 import { useDispatch } from 'react-redux';
@@ -25,43 +26,42 @@ function DashboardLayout({ children }) {
 
   return (
     <div>
-      <div className="container">
-        <div className={styles.homeContainer}>
+      <div className={styles.homeContainer}>
 
-          <div className={styles.homeContainer_left}>
+        <div className={styles.homeContainer_left}>
 
-            <div onClick={() => {
-              router.push('/dashboard');
-            }}className={styles.sideBarOption}>
+          <div onClick={() => {
+            router.push('/dashboard');
+          }} className={styles.sideBarOption}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                 <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+              <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
             </svg>
             <p>Scroll</p>
-            </div>
+          </div>
 
-            <div onClick={() => {
-              router.push('/discover');
-            }}className={styles.sideBarOption}>
+          <div onClick={() => {
+            router.push('/discover');
+          }} className={styles.sideBarOption}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                 <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+              <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
             </svg>
             <p>Discover</p>
-            </div>
+          </div>
 
-            <div onClick={() => {
-              router.push('/my_connections');
-            }}className={styles.sideBarOption}>
+          <div onClick={() => {
+            router.push('/my_connections');
+          }} className={styles.sideBarOption}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
             </svg>
 
             <p>My Connections</p>
-            </div>
-
           </div>
 
-          <div className={styles.homeContainer_feedContainer}>
-            {children}
+        </div>
+
+        <div className={styles.homeContainer_feedContainer}>
+          {children}
         </div>
 
         <div className={styles.homeContainer_extraContainer}>
@@ -74,54 +74,48 @@ function DashboardLayout({ children }) {
               </div>
             )
           })} */}
-           <h3 className={styles.topProfileHeading}>Top Profiles</h3>
+          <h3 className={styles.topProfileHeading}>Top Profiles</h3>
 
-{authState.all_profiles_fetched && authState.all_users.map((profile) => (
-  <div key={profile._id} className={styles.extraContainer_profile}
-    onClick={() => router.push(`/profile/${profile.userId._id}`)}>
-    
-    {/* <div className={styles.profileAvatar}>
+          {authState.all_profiles_fetched && authState.all_users.map((profile) => (
+            <div key={profile._id} className={styles.extraContainer_profile}
+              onClick={() => router.push(`/profile/${profile.userId._id}`)}>
+
+              {/* <div className={styles.profileAvatar}>
       {profile.userId.profilePicture
         ? <img src={profile.userId.profilePicture} alt={profile.userId.name} />
         : <span>{profile.userId.name?.charAt(0).toUpperCase()}</span>
       }
     </div> */}
-    <div className={styles.profileAvatar}>
-  {profile.userId.profilePicture && profile.userId.profilePicture !== "default.jpg" ? (
-    <img
-      src={`http://localhost:5000/uploads/${profile.userId.profilePicture}`}
-      alt={profile.userId.name}
-      onError={(e) => {
-        e.target.style.display = 'none';
-        e.target.nextSibling.style.display = 'flex';
-      }}
-    />
-  ) : null}
-  <span style={{ 
-    display: profile.userId.profilePicture && profile.userId.profilePicture !== "default.jpg" 
-      ? 'none' 
-      : 'flex' 
-  }}>
-    {profile.userId.name?.charAt(0).toUpperCase()}
-  </span>
-</div>
+              <div className={styles.profileAvatar}>
+                {profile.userId.profilePicture && profile.userId.profilePicture !== "default.jpg" ? (
+                  <img
+                    src={`${BASE_URL}/${profile.userId.profilePicture}`}
+                    alt={profile.userId.name}
+                    onError={(e) => {
+                      e.target.style.display = 'none';
+                      e.target.nextSibling.style.display = 'flex';
+                    }}
+                  />
+                ) : null}
+                <span style={{
+                  display: profile.userId.profilePicture && profile.userId.profilePicture !== "default.jpg"
+                    ? 'none'
+                    : 'flex'
+                }}>
+                  {profile.userId.name?.charAt(0).toUpperCase()}
+                </span>
+              </div>
 
-    <div className={styles.profileInfo}>
-      <p className={styles.profileName}>{profile.userId.name}</p>
-      <p className={styles.profileUsername}>@{profile.userId.username}</p>
-    </div>
+              <div className={styles.profileInfo}>
+                <p className={styles.profileName}>{profile.userId.name}</p>
+                <p className={styles.profileUsername}>@{profile.userId.username}</p>
+              </div>
 
-  </div>
-))}
+            </div>
+          ))}
         </div>
-
-        </div>
-
-        
 
       </div>
-
-
 
     </div>
   )
--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -62,20 +62,22 @@ export default function Dashboard() {
 
             <textarea onChange={(e) => setPostContent(e.target.value)} value={postContent} className={styles.textareaOfContent} placeholder={"What's in your mind?"}name='' id='' ></textarea>
 
-            <label htmlFor='fileUpload'>
-              <div className={styles.Fab}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                </svg>
-              </div>
-            </label>
+            <div className={styles.inputActions}>
+              <label htmlFor='fileUpload'>
+                <div className={styles.Fab}>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                  </svg>
+                </div>
+              </label>
 
-            <input onChange={(e) => setFileContent(e.target.files[0])} type='file' hidden id='fileUpload' />
-            {postContent.length > 0 &&
-            <div onClick={handleUpload} className={styles.uploadButton}>
-            Post
+              <input onChange={(e) => setFileContent(e.target.files[0])} type='file' hidden id='fileUpload' />
+              {postContent.length > 0 &&
+              <button onClick={handleUpload} className={styles.uploadButton}>
+              Post
+              </button>
+              }
             </div>
-            }
           </div>
 
           <div className={styles.postContainer}>
@@ -84,10 +86,10 @@ export default function Dashboard() {
               <div key={post._id} className={styles.singleCard}>
 
                 <div className={styles.singleCard_profile}>
-                  <img className={styles.singleCard_profile_img} src={`${BASE_URL}/${post?.userId?.profilePicture}`} alt=''/> <br></br>
-                  <div>
-                    <div style={{display: "flex", gap: "1,2rem", justifyContent: "space-between", }}>
-                      <p style={{fontWeight: 'bold'}}>{post?.userId?.name}</p>
+                  <img className={styles.singleCard_profile_img} src={`${BASE_URL}/${post?.userId?.profilePicture}`} alt=''/>
+                  <div style={{ flex: 1 }}>
+                    <div className={styles.postHeader}>
+                      <p>{post?.userId?.name}</p>
 
                       {
                         post.userId?._id === authState.user?.userId?._id &&
@@ -95,20 +97,22 @@ export default function Dashboard() {
                           await dispatch(deletePost({post_id: post._id}));
                           await dispatch(getAllPosts());
                         }}
-                        style={{cursor: "pointer"}}>
-                          <svg style={{height: "1.2em", color: "red"}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        className={styles.deleteIcon}>
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
                           <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                           </svg>
                         </div>
                       }
 
                     </div>
-                    <p style={{color: "gray", paddingTop: "5px"}}>@{post?.userId?.username}</p>
-                    <p style={{paddingTop: "1.3rem"}}>{post?.body}</p>
+                    <p className={styles.postUsername}>@{post?.userId?.username}</p>
+                    <p className={styles.postBody}>{post?.body}</p>
+                  </div>
+                </div>
 
-                    <div className={styles.singleCard_image}>
-                      <img src={`${BASE_URL}/${post?.media}`} alt=''/>
-                    </div>
+                <div className={styles.singleCard_image}>
+                  <img src={`${BASE_URL}/${post?.media}`} alt=''/>
+                </div>
 
                     <div className={styles.optionsContainer}>
 
@@ -145,9 +149,8 @@ export default function Dashboard() {
                       </div>
 
                     </div>
-                  </div>
-
-                </div>
+                  
+                
                 
             </div>
             );
@@ -167,27 +170,26 @@ export default function Dashboard() {
               e.stopPropagation();
             }}
             className={styles.allCommentsContainer}>
-              {postState.comments.length === 0 && <h3>No Comments</h3>}
+              <div className={styles.commentsList}>
+                {postState.comments.length === 0 && <h3>No Comments</h3>}
 
-              {postState.comments.length !== 0 && 
-              <div>
-                {postState.comments.map((comment, index) => {
-                  return (
-                    <div className={styles.singleComment} key={comment._id}>
-                      <div className={styles.singleComment_profileContainer}>
-                        <img src={`${BASE_URL}/${comment?.userId?.profilePicture}`} alt=''/>
-                        <div>
-                          <p style={{ fontWeight: 550, fontSize: "1.2rem"}}>{comment.userId.name}</p>
-                          <p style={{fontWeight: 100, fontSize: "0.9rem"}}>@{comment.userId.username}</p>
+                {postState.comments.length !== 0 && 
+                  postState.comments.map((comment, index) => {
+                    return (
+                      <div className={styles.singleComment} key={comment._id}>
+                        <div className={styles.singleComment_profileContainer}>
+                          <img src={`${BASE_URL}/${comment?.userId?.profilePicture}`} alt=''/>
+                          <div>
+                            <h4>{comment.userId.name}</h4>
+                            <p>@{comment.userId.username}</p>
+                          </div>
                         </div>
+                        <p>{comment.body}</p>
                       </div>
-                      <p>{comment.body}</p>
-                      
-                    </div>
-                  )
-                })}
+                    )
+                  })
+                }
               </div>
-              }
 
               <div className={styles.postCommentContainer}>
                 <input type='' value={commentText} onChange={(e) => 
